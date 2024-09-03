6ec846eaae376aa2224f4060092b30fb6c3dd024
abdul-webcraft <abdul@webcraftsolutions.in>
2024-09-03 16:31:33 +0530

    add dowhile and while code in main.java
    Co-Authored-By: yogendrawebcraft <180271046+yogendrawebcraft@users.noreply.github.com>
    Co-Authored-By: Abhinavwebcraft <180271265+abhinavwebcraft@users.noreply.github.com>

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,7 +1,24 @@
 public class ForLoop {
     public static void main(String[] args) {
+
+        System.out.println("Using For Loop.......");
         for (int i = 1; i < 10; i++) {
+
             System.out.println(i);
         }
+        int i=1;
+        System.out.println("Using While Loop");
+
+        while (i<10){
+            System.out.println(i);
+            i++;
+
+        }
+        System.out.println("Using Do While Loop");
+
+        do {
+            System.out.println(i);
+            i++;
+        }while (i<15);
     }
 }
